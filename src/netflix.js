029afe818efab9bd6583c9a4db32616a4be80c00
ASHISH TIWARI <abhitiwari9857@gmail.com>
2024-07-19 17:31:54 +0530

    Initial commit

---
+++ src/netflix.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('.signin-button').addEventListener('click', function () {
+        alert('Site is not connected to the main page.');
+    });
+});
